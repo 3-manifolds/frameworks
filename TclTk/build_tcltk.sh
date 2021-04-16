4f7aa54e4228849af156bb58bd5c65cdc4895821
Nathan Dunfield <nathan@dunfield.info>
2021-04-15 21:24:15 -0500

    build_tcltk.sh: fixed typo in command mkdir

--- TclTk/build_tcltk.sh
+++ TclTk/build_tcltk.sh
@@ -1,6 +1,6 @@
 BASE=`pwd`
 export CFLAGS="-mmacosx-version-min=10.9"
-mdkir -p ${BASE}/dist
+mkdir -p ${BASE}/dist
 make -j6 -C Tcl/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
      DYLIB_INSTALL_DIR=@executable_path/../Frameworks/Tcl.framework/Versions/8.6/Tcl
 make -j6 -C Tk/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
