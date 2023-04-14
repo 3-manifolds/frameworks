13f978a1d81e1b1df7b5d29d6e0b64fcf4421893
Marc Culler <culler@users.noreply.github.com>
2023-04-14 07:25:32 -0500

    Use curl instead of wget since it ships with macOS

--- TclTk/fetch_tcltk.sh
+++ TclTk/fetch_tcltk.sh
@@ -4,10 +4,12 @@ set -e
 TCL_VERSION=8.6.13
 TK_VERSION=8.6.13
 
-mkdir Tcl Tk
+curl -OL https://prdownloads.sourceforge.net/tcl/tcl$TCL_VERSION-src.tar.gz
+curl -OL https://prdownloads.sourceforge.net/tcl/tk$TK_VERSION-src.tar.gz
 
-wget -O tcl-src.tar.gz https://prdownloads.sourceforge.net/tcl/tcl$TCL_VERSION-src.tar.gz
-wget -O tk-src.tar.gz https://prdownloads.sourceforge.net/tcl/tk$TK_VERSION-src.tar.gz
+tar xf tcl$TCL_VERSION-src.tar.gz
+tar xf tk$TK_VERSION-src.tar.gz
 
-tar xf tcl-src.tar.gz --directory=Tcl --strip-components=1
-tar xf tk-src.tar.gz --directory=Tk --strip-components=1
+rm -rf Tcl Tk
+mv tcl$TCL_VERSION Tcl
+mv tk$TK_VERSION Tk
