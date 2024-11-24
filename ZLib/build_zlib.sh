7874f550b725f63ae6d3e8e9e1f121d1e664740d
Marc Culler <culler@users.noreply.github.com>
2024-11-23 18:10:27 -0600

    Change zlib url.

--- ZLib/build_zlib.sh
+++ ZLib/build_zlib.sh
@@ -8,7 +8,7 @@ LIBZ=libz.${LONG_VERSION}.dylib
 SRC_DIR=zlib-${LONG_VERSION}
 SRC_ARCHIVE=zlib-${LONG_VERSION}.tar.gz
 RSRC_DIR=${FRAMEWORK_BUILD}/Versions/${VERSION}/Resources
-URL=https://zlib.net/zlib-1.3.tar.gz
+URL=https://github.com/madler/zlib/releases/download/v1.3/zlib-1.3.tar.gz
 HASH=ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e
 
 if ! [ -e ${SRC_ARCHIVE} ]; then
