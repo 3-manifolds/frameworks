978e86e8b44d710968f7ce4ebb41d909e92f10d7
Marc Culler <culler@users.noreply.github.com>
2025-01-20 11:28:12 -0600

    Need to download Tcl and Tk source before building.

--- TclTk/build_tcltk.sh
+++ TclTk/build_tcltk.sh
@@ -1,6 +1,7 @@
 set -e
 cd `dirname $0`
 BASE=`pwd`
+. fetch_tcltk.sh
 export CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.9"
 mkdir -p ${BASE}/dist
 make -j6 -C Tcl/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
